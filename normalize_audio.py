403c840791cb0a1a284db97bbb2eebd6ebac8071
sharkie <y.schriefers@gmail.com>
2025-05-15 22:24:37 +0200

    added silence removal + fade

--- normalize_audio.py
+++ normalize_audio.py
@@ -70,17 +70,44 @@ async def get_max_volume(
     if not match:
         raise RuntimeError("Could not find max_volume in ffmpeg output.")
     return float(match.group(1))
+async def get_duration_seconds(input_file:str) -> float:
+    """Returns the duration of given audio file in seconds.
+
+    Args:
+        input_file (str): Path to filoe
+
+    Returns:
+        float: Duration of audio in seconds
+    """
+    cmd = [
+        'ffprobe', '-v', 'quiet',
+        '-print_format',
+        'compact=print_section=0:nokey=1:escape=csv',
+        '-show_entries',
+        'format=duration',
+        input_file
+    ]
+    async with semaphore_ffmpeg:
+        result = await asyncio.to_thread(
+            subprocess.run,
+            cmd,
+            capture_output=True,
+            text=True,
+            encoding= 'utf-8'
+        )
+    return float(result.stdout.strip())
 
 async def normalize(
         input_file:str,
         target_dBFS:float, #pylint:disable=invalid-name
-        target_bitrate:str = '128k'
-    ) -> None:
-    """Normalize the volume of the audio file recursively to avoid clipping."""
-    _, extension = os.path.splitext(input_file)
-    clip_file = input_file + "_temp.mp3"
-    output_tmp = input_file + "_out.mp3"
-    output_file = input_file.replace(extension, '.mp3')
+    ) -> str:
+    """Normalize the volume of the audio file recursively to avoid clipping.
+    Returns:
+        str: Path to output file
+    """
+    # base, _ = os.path.splitext(input_file)
+    clip_file = input_file + ".cliptmp.wav"
+    output_file = input_file + '.normalized.wav'
 
     max_volume = await get_max_volume(input_file)
     is_clipped = round(max_volume, 1) == 0.0
@@ -89,7 +116,7 @@ async def normalize(
         cmd = [
             "ffmpeg", "-y", "-i", input_file,
             "-af", f"volume={clip_test_dB}dB,volumedetect",
-            "-c:a", "libmp3lame", "-b:a", target_bitrate,
+            # "-c:a", "libmp3lame", "-b:a", '128k',
             clip_file
         ]
         async with semaphore_ffmpeg:
@@ -110,29 +137,100 @@ async def normalize(
             "ffmpeg", "-y", "-i", input_file,
             '-loglevel', 'error',
             "-af", f"volume={target_dBFS - max_volume}dB,volumedetect",
-            "-c:a", "libmp3lame", "-b:a", target_bitrate,
-            output_tmp
+            output_file
         ])
 
-    backup_file = input_file + '.tmp'
-    await move_file(input_file, backup_file)
-    await move_file(output_tmp, output_file)
-    for file in (clip_file, backup_file):
+    for file in (clip_file, ):
         try:
             await delete_file(file)
         except FileNotFoundError:
             pass
+    return output_file
+async def remove_silence(input_file:str) -> str:
+    """Normalize the volume of the audio file recursively to avoid clipping.
+    Returns:
+        str: Path to output file
+    """
+    silenceremove_file = input_file + ".silenceremove.wav"
+    output_file = input_file + '.remove_silence.wav'
+
+    # variable names according to https://ffmpeg.org/ffmpeg-filters.html#silenceremove
+    # these are named for the removal of silence at the beginning, but are used for the end as well
+    start_duration = 0
+    start_threshold_dB = -30 #pylint:disable=invalid-name
+    start_silence = 0.2
+
+    silenceremove_parameters = f'1:{start_duration}:{start_threshold_dB}dB:{start_silence}'
+    async with semaphore_ffmpeg:
+        print(f'Removing silence {input_file} ...')
+        await asyncio.to_thread(subprocess.run, [
+            "ffmpeg", "-y", "-i", input_file,
+            '-loglevel', 'error',
+            "-af", (
+                        f'silenceremove={silenceremove_parameters},'
+                        'areverse,'
+                        f'silenceremove={silenceremove_parameters},'
+                        'areverse'
+                    ),
+            silenceremove_file
+        ])
+
+
+    duration_seconds:float = await get_duration_seconds(silenceremove_file)
+    async with semaphore_ffmpeg:
+        print(f'Applying fades {input_file} ...')
+        await asyncio.to_thread(subprocess.run, [
+            "ffmpeg", "-y", "-i", silenceremove_file,
+            '-loglevel', 'error',
+            "-af", (
+                        f'afade=t=in:ss=0:d={start_silence},'
+                        f'afade=t=out:st={duration_seconds - start_silence}:d={start_silence}'
+                    ),
+            output_file
+        ])
+
+    for file in (silenceremove_file, ):
+        try:
+            await delete_file(file)
+        except FileNotFoundError:
+            pass
+    return output_file
+
 async def process_file(input_file:str) -> None:
     """Main function that runs all the processing functions you want on a file.
 
     Args:
         input_file (str): Path to file
     """
-    await normalize(
-        input_file,
-        float(config['DEFAULTS']['targetdBFS']),
-        config['DEFAULTS']['bitrate']
+    base, _ = os.path.splitext(input_file)
+    final_output = base + '.mp3'
+    final_output_tmp = final_output + '.tmp.mp3'
+    cleanup_files:list[str] = [input_file]
+
+    input_file = await remove_silence(
+        input_file
     )
+    cleanup_files.append(input_file)
+
+    input_file = await normalize(
+        input_file,
+        float(config['DEFAULTS']['targetdBFS'])
+    )
+    cleanup_files.append(input_file)
+
+
+    await asyncio.to_thread(subprocess.run, [
+        "ffmpeg", "-y", "-i", input_file,
+        '-loglevel', 'error',
+        "-c:a", "libmp3lame", "-b:a", config['DEFAULTS']['bitrate'],
+        final_output_tmp
+    ])
+    for file in cleanup_files:
+        try:
+            await delete_file(file)
+        except FileNotFoundError:
+            pass
+    await move_file(final_output_tmp, final_output)
 
 async def main(files:list[str]) -> None:
     """Processes all files given.
